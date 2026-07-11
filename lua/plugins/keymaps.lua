abd782faaaddb1a7c7129533380157b07ccc3c27
thangatamil <thangatamil518@gmail.com>
2026-07-11 20:22:03 +0530

    update exit insert with "Ctrl + j" to reduce wrist pain :)

--- lua/plugins/keymaps.lua
+++ lua/plugins/keymaps.lua
@@ -10,4 +10,4 @@ vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window" })
 
-vim.keymap.set("i", "<Tab>j", "<Esc>", { desc = "Exit Insert Mode" })
+vim.keymap.set("i", "<C-j>", "<Esc>", { desc = "Exit Insert Mode" })
